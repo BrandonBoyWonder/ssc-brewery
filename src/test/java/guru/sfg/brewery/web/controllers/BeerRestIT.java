4503199e698d61be0d648b5cf9e18e8ea8dea7a9
BrandonBoyWonder <brandon.bob.moore@gmail.com>
2020-10-27 07:09:06 +0000

    added spring security custom auth filter

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestIT.java
@@ -3,11 +3,33 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class BeerRestIT  extends BaseIT{
+
+    @Test
+    void deleteBeerUrl() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb7e46-932d-4ab8-a903-b0131fe03686")
+                .param("Api-key", "spring").param("Api-Secert", "guru"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerBadCreds() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb7e46-932d-4ab8-a903-b0131fe03686")
+                .header("Api-key", "spring").header("Api-Secert", "guruXXXX"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb7e46-932d-4ab8-a903-b0131fe03686")
+        .header("Api-key", "spring").header("Api-Secert", "guru"))
+        .andExpect(status().isOk());
+    }
 
     @Test
     void findBeers() throws Exception {
